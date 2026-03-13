9f09f4a91c1cd0cbee67ea65416c27675b9d3879
Utkarsha Vijay Salve😁 <usalve@gitam.in>
2026-03-13 19:52:11 +0530

    Create CalculatorTest.java

---
+++ src/test/java/com/demo/CalculatorTest.java
@@ -0,0 +1,14 @@
+package com.demo;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class CalculatorTest {
+
+    @Test
+    public void testAdd(){
+        Calculator c = new Calculator();
+        assertEquals(5, c.add(2,3));
+    }
+
+}
